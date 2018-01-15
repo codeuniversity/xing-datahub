2aff9ce3f0a2aebf2fd8c82cf025e2bb87748c69
alexmorten <alexandrius.martinus@gmx.de>
2018-01-15 18:20:52 +0100

    add protobuff

--- consumer.py
+++ consumer.py
@@ -1,16 +1,20 @@
-from kafka import KafkaConsumer
+from confluent_kafka import Consumer, KafkaError
+import sys
+# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
+sys.path.append('build')
+import user_pb2
 
-
-class Consumer():
-    def start(self):
-        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                                 auto_offset_reset='earliest',
-                                 consumer_timeout_ms=1000)
-        consumer.subscribe(['data'])
-        while True:
-            for message in consumer:
-                print(message)
-
-        consumer.close()
-
-Consumer().start()
+c = Consumer({'bootstrap.servers': 'localhost:9092', 'group.id': 'bla',
+              'default.topic.config': {'auto.offset.reset': 'smallest'}})
+c.subscribe(['users'])
+running = True
+while running:
+    msg = c.poll()
+    if not msg.error():
+        user = user_pb2.User()
+        user.ParseFromString(msg.value())
+        print(user.id, ' ',user.first_name, ' ', user.last_name)
+    elif msg.error().code() != KafkaError._PARTITION_EOF:
+        print(msg.error())
+        running = False
+c.close()
--- producer.py
+++ producer.py
@@ -1,17 +1,40 @@
-from kafka import KafkaProducer
-import time
+from confluent_kafka import Producer
+import json
+import sys
+# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
+sys.path.append('build')
+import user_pb2
 
+user_data = {
+    "id": 1,
+    "first_name": "Nick",
+    "last_name": "Recobra",
+    "haves": ["data engineering", "scala"],
+    "wants": ["machine learning", "clojure"],
+    "gender": ["male"],
+    "languages": ["ru", "en"],
+    "business_address": {
+        "zipcode": "22607",
+        "country": "Germany",
+        "city": "Hamburg",
+        "street": "Dammtorstrasse 30"
+    },
+    "primary_company": {
+        "title": "Data Engineer",
+        "industry": "IT",
+        "name": "XING SE"
+    }
+}
+msg = json.dumps(user_data)
+user_dict = json.loads(msg)
 
-class Producer():
-    def start(self):
-        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+user = user_pb2.User()
+user.id = user_dict['id']
+user.first_name = user_dict['first_name']
+user.last_name = user_dict['last_name']
 
-        for i in range(3):
-            producer.send('data', 'test \#{}'.format(i).encode('utf-8'))
-            print(i)
-            time.sleep(0.5)
+s = user.SerializeToString()
 
-        producer.close()
-
-
-Producer().start()
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+p.produce('users', s)
+p.flush()
