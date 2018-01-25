2d962226d48b462b8e8c45a1c74a2d5c3259efd8
alexmorten <alexandrius.martinus@gmx.de>
2018-01-25 17:09:35 +0100

    let the producer receive posts

---
+++ mock_poster.py
@@ -0,0 +1,23 @@
+import aiohttp
+import asyncio
+import async_timeout
+
+def get_json_string(file):
+    return file.readline()
+
+async def post(session, url, msg):
+    print(msg)
+    with async_timeout.timeout(10):
+        async with session.post(url, data=msg) as response:
+            return await response.text()
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        f = open('../users/data/users.jsonl', 'r')
+        msg = get_json_string(f)
+        while not msg is None and not msg == '':
+            await post(session, 'http://localhost:8080/users', msg)
+            msg = get_json_string(f)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
--- producer.py
+++ producer.py
@@ -1,38 +1,31 @@
 from confluent_kafka import Producer
 import json
-import aiohttp
+from aiohttp import web
 import asyncio
 import async_timeout
 import sys
 # the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-sys.path.append('build')
 from build import user_pb2
+from google.protobuf import json_format
 
 p = Producer({'bootstrap.servers': 'localhost:9092'})
 
+def serialize_user(json_str):
+    return json_format.Parse(json_str, user_pb2.User()).SerializeToString()
 
 def push_to_kafka(msg):
-    user_dict_array = json.loads(msg)
-    for user_dict in user_dict_array:
-        user = user_pb2.User()
-        user.id = user_dict['id']
-        user.first_name = user_dict['first_name']
-        user.last_name = user_dict['last_name']
-        kafka_message = user.SerializeToString()
-        p.produce('users', kafka_message)
+    kafka_message = serialize_user(msg)
+    print(kafka_message)
+    p.produce('users', kafka_message)
     p.flush()
 
 
-async def fetch(session, url):
-    with async_timeout.timeout(10):
-        async with session.get(url) as response:
-            return await response.text()
+async def handle_users(request):
+    user_str = await request.text()
+    push_to_kafka(user_str)
+    return web.Response()
 
+app = web.Application()
+app.router.add_post('/users', handle_users)
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        msg = await fetch(session, 'http://localhost:8080/users')
-        push_to_kafka(msg)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+web.run_app(app)
