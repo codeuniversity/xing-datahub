d2c8959e1b4323ab2b87a1bbfafdad1197de43a0
alexmorten <alexandrius.martinus@gmx.de>
2018-01-25 17:09:01 +0100

    make consumer cancelable

--- consumer.py
+++ consumer.py
@@ -1,16 +1,18 @@
 from confluent_kafka import Consumer, KafkaError
 import sys
 # the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-sys.path.append('build')
 from build import user_pb2
 
 c = Consumer({'bootstrap.servers': 'localhost:9092', 'group.id': 'bla',
               'default.topic.config': {'auto.offset.reset': 'smallest'}})
 c.subscribe(['users'])
+
 running = True
 while running:
-    msg = c.poll()
-    if not msg.error():
+    msg = c.poll(2)
+    if msg is None:
+        pass
+    elif not msg.error():
         user = user_pb2.User()
         user.ParseFromString(msg.value())
         print(user.id, ' ',user.first_name, ' ', user.last_name)
