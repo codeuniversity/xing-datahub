9e0c37c055569d0632909e1af169167446a52a11
alexmorten <alexandrius.martinus@gmx.de>
2018-02-04 17:12:00 +0100

    handle new dataset

--- hive_handler.py
+++ hive_handler.py
@@ -1,35 +1,67 @@
 from pyhive import hive
 
+cursor = hive.connect('localhost').cursor()
+
+def create_table(name, schema_string):
+  query = """
+  CREATE TABLE IF NOT EXISTS {}
+  ({}) STORED AS ORC
+  """.format(name, schema_string)
+  cursor.execute(query)
+
 user_schema_string = """
   id INT,
-  first_name STRING,
-  last_name STRING,
-  gender STRING,
-  wants Array<STRING>,
-  haves Array<STRING>,
-  languages Array<STRING>,
-  business_address struct<country: STRING, zipcode: STRING, city: STRING, street: STRING>,
-  primary_company struct<title: STRING, name: STRING>
+  jobroles Array<INT>,
+  career_level INT,
+  discipline_id INT,
+  industry_id INT,
+  country STRING,
+  region int,
+  experience_n_entries_class INT,
+  experience_years_experience INT,
+  experience_years_in_current INT,
+  edu_degree INT,
+  edu_fieldofstudies Array<INT>,
+  wtcj INT,
+  premium INT
   """
 
-connection_schema_string = """
-  a INT,
-  b INT
+create_table('users', user_schema_string)
+
+item_schema_string = """
+  id INT,
+  title INT,
+  career_level INT,
+  discipline_id INT,
+  industry_id INT,
+  country STRING,
+  is_payed INT,
+  region INT,
+  latitude FLOAT,
+  longitude FLOAT,
+  employment INT,
+  tags Array<int>,
+  created_at INT
   """
+create_table('items', item_schema_string)
 
-users_create_query = """
-  CREATE TABLE IF NOT EXISTS users
-  ({}) STORED AS ORC
-  """.format(user_schema_string)
+interaction_schema_string = """
+  user_id INT,
+  item_id INT,
+  interaction_type INT,
+  created_at INT
+  """
+create_table('interactions', interaction_schema_string)
 
-connections_create_query = """
-  CREATE TABLE IF NOT EXISTS connections
-  ({}) STORED AS ORC
-  """.format(connection_schema_string)
+target_user_schema_string = """
+  user_id INT
+"""
+create_table('target_users', target_user_schema_string)
 
-cursor = hive.connect('localhost').cursor()
-cursor.execute(users_create_query)
-cursor.execute(connections_create_query)
+target_item_schema_string = """
+  item_id INT
+"""
+create_table('target_items', target_item_schema_string)
 
 def create_csv_table(name, location_prefix, schema_string):
     create_table = """
