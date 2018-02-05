8ab0a857071a8a4c98427a161bd36f21ac24cffd
alexmorten <alexandrius.martinus@gmx.de>
2018-02-05 20:14:54 +0100

    don't die on file errors

--- csv_export_handler.py
+++ csv_export_handler.py
@@ -9,11 +9,14 @@ class ExportHandler(object):
     self.schema_string = schema_string
 
   def commit(self, filepath, filename):
-    remote_location = self.name + "/{}b/data".format(filename)
-    hdfs_helpers.put_in_hdfs(remote_location, filepath)
-    self._handle_tables(filename)
-    # TODO: remove csv file from hdfs too
-    os.remove(filepath)
+    try:
+      remote_location = self.name + "/{}b/data".format(filename)
+      hdfs_helpers.put_in_hdfs(remote_location, filepath)
+      self._handle_tables(filename)
+      # TODO: remove csv file from hdfs too
+      os.remove(filepath)
+    except Exception as err:
+      print(err)
 
   def _handle_tables(self, filename):
     hive_handler.create_csv_table(filename, self.name, self.schema_string)
