6b410be5a74688a12c184a9bc179c709a88b4b2b
alexmorten <alexandrius.martinus@gmx.de>
2018-02-16 23:37:12 +0100

    add prometheus metrics to python batch inserter

--- consumer.py
+++ consumer.py
@@ -1,8 +1,13 @@
 from confluent_kafka import Consumer, KafkaError
+from prometheus_client import start_http_server, Counter
 import sys
 from build import protocol_pb2
 import hive_handler
 from csv_export_handler import ExportHandler
+from prometheus_client import Counter
+
+INSERTED = Counter('inserted_batches', 'counts the inserted batches by record type', ['record'])
+
 c = Consumer({'bootstrap.servers': 'localhost:9092', 'group.id': 'written file consumer',
               'default.topic.config': {'auto.offset.reset': 'smallest'}})
 c.subscribe(['written_files'])
@@ -16,7 +21,9 @@ target_item_exporter = ExportHandler(name='target_items', schema_string=hive_han
 def handle_file(msg):
     csvInfo = protocol_pb2.WrittenCSVInfo()
     csvInfo.ParseFromString(msg.value())
+
     t = csvInfo.recordType
+    unsupported = False
     if t == "users":
         user_exporter.commit(csvInfo.filepath, csvInfo.filename)
     elif t == "items":
@@ -29,6 +36,9 @@ def handle_file(msg):
         target_item_exporter.commit(csvInfo.filepath, csvInfo.filename)
     else:
         print("recod type ", t, " not supported")
+        unsupported = True
+    if not unsupported:
+        INSERTED.labels(t).inc()
 
 def handle_connection(msg):
     connection = protocol_pb2.Connection()
@@ -41,16 +51,19 @@ def handle_message(msg):
     else:
         print(msg.topic(), ' not handled')
 
-running = True
-while running:
-    msg = c.poll(2)
-    if msg is None:
-        pass
-    elif not msg.error():
-        handle_message(msg)
-    elif msg.error().code() == KafkaError._PARTITION_EOF:
-        pass
-    else:
-        print(msg.error())
-        running = False
-c.close()
+if __name__ == '__main__':
+    start_http_server(3002)
+
+    running = True
+    while running:
+        msg = c.poll(2)
+        if msg is None:
+            pass
+        elif not msg.error():
+            handle_message(msg)
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            pass
+        else:
+            print(msg.error())
+            running = False
+    c.close()
