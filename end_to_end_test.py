91407495d55d94e5db657de569210565c26c9647
alexmorten <alexandrius.martinus@gmx.de>
2018-03-28 11:41:44 +0200

    send each user

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -7,11 +7,14 @@ TOKEN = os.getenv('token', '')
 class EndpointTestCase(unittest.TestCase):
   def test(self):
     users = [{ 'id':42, 'country': 'de' }, { 'id':42, 'country': 'de' }]
-    requests.post('http://localhost:3000/users', json=users, headers={'access-token': TOKEN})
+    for user in users:
+      requests.post('http://localhost:3000/users', json=user, headers={'access-token': TOKEN})
 
     time.sleep(30)
     resp = requests.get('http://localhost:3003/users', headers={'access-token': TOKEN})
     returned_users = resp.json()
+    print(returned_users)
+    print(type returned_users)
     self.assertEqual(len(returned_users), 2)
     self.assertEqual(returned_users, users)
 unittest.main()
