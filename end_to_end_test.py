69794b1408fd0c62b662b78bd84ef000a7ecd858
alexmorten <alexandrius.martinus@gmx.de>
2018-03-28 12:29:34 +0200

    use correct env

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -3,7 +3,7 @@ import json
 import requests
 import time
 import os
-TOKEN = os.getenv('token', '')
+TOKEN = os.getenv('TOKEN', '')
 class EndpointTestCase(unittest.TestCase):
   def test(self):
     users = [{ 'id':42, 'country': 'de' }, { 'id':42, 'country': 'de' }]
