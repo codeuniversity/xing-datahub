eeb02adea672333d6c35ff0086610201e1385272
alexmorten <alexandrius.martinus@gmx.de>
2018-03-28 11:26:24 +0200

    url for post

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv('token', '')
 class EndpointTestCase(unittest.TestCase):
   def test(self):
     users = [{ 'id':42, 'country': 'de' }, { 'id':42, 'country': 'de' }]
-    requests.post('localhost:3000/users', json=users, headers={'access-token': TOKEN})
+    requests.post('http://localhost:3000/users', json=users, headers={'access-token': TOKEN})
 
     time.sleep(30)
     resp = requests.get('http://localhost:3003/users', headers={'access-token': TOKEN})
