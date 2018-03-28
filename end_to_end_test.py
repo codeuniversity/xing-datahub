95908aa644f24b980098b582c1a7c8968d7722d2
alexmorten <alexandrius.martinus@gmx.de>
2018-03-28 14:20:19 +0200

    use more time

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -10,11 +10,14 @@ class EndpointTestCase(unittest.TestCase):
     for user in users:
       resp = requests.post('http://localhost:3000/users', json=user, headers={'access-token': TOKEN})
       self.assertEqual(resp.status_code, 200)
-    time.sleep(60)
+    time.sleep(90)
     resp = requests.get('http://localhost:3003/users', headers={'access-token': TOKEN})
     returned_users = resp.json()
     print(returned_users)
     print(type(returned_users))
     self.assertEqual(len(returned_users), 2)
-    self.assertEqual(returned_users, users)
+    self.assertDictContainsSubset(users[0], returned_users[0])
+    self.assertDictContainsSubset(users[1], returned_users[1])
+
+time.sleep(10)
 unittest.main()
