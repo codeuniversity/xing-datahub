5e42e5d4755a4c34f18889491d0a5459da1cb084
alexmorten <alexandrius.martinus@gmx.de>
2018-03-28 14:28:16 +0200

    compare right users

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -7,6 +7,7 @@ TOKEN = os.getenv('TOKEN', '')
 class EndpointTestCase(unittest.TestCase):
   def test(self):
     users = [{ 'id':'42', 'country': 'de' }, { 'id':'42', 'country': 'de' }]
+    expected_users = [{ 'id':42, 'country': 'de' }, { 'id':42, 'country': 'de' }]
     for user in users:
       resp = requests.post('http://localhost:3000/users', json=user, headers={'access-token': TOKEN})
       self.assertEqual(resp.status_code, 200)
@@ -16,8 +17,8 @@ class EndpointTestCase(unittest.TestCase):
     print(returned_users)
     print(type(returned_users))
     self.assertEqual(len(returned_users), 2)
-    self.assertDictContainsSubset(users[0], returned_users[0])
-    self.assertDictContainsSubset(users[1], returned_users[1])
+    self.assertDictContainsSubset(expected_users[0], returned_users[0])
+    self.assertDictContainsSubset(expected_users[1], returned_users[1])
 
 time.sleep(10)
 unittest.main()
